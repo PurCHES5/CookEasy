62ca7f46b29c68a5bb6f13b20fb9ed6951eec713
PurCHES5 <wuzhiqi021221@gmail.com>
2022-03-16 18:00:06 +0800

    project initialization

---
+++ CookEasy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CookEasy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CookEasy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CookEasy/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CookEasy.Models;
+using CookEasy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookEasy.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
