618b7c2b7b0bc3af0fcce77f5bee730b019afe36
CaesarLiew <lh997072@gmail.com>
2022-03-26 17:18:00 +0800

    register page

--- CookEasy/App.xaml.cs
+++ CookEasy/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CookEasy
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new RegisterPage();
         }
 
         protected override void OnStart()
