d6d4f2069cf27d621dfa59179bbe5741f4d4e405
Qi Shengen <cest.andrew@outlook.com>
2022-03-27 21:38:28 +0800

    home page

---
+++ CookEasy/Models/RecipeProp.cs
@@ -0,0 +1,48 @@
+﻿
+namespace CookEasy.Models
+{
+    public class RecipeProp
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        /// <summary>
+        /// 0 = Easy, 1 = Advanced
+        /// </summary>
+        public int Difficulty { get; set; }
+        /// <summary>
+        /// 0 = Easy, 1 = Advanced, 2 = Both
+        /// </summary>
+        public int DifficultiesAvail { get; set; } 
+        public int Likes { get; set; }
+        public string CardColor { get; set; }
+
+        public bool EasyAvailable       // these codes are for easier Binding in xaml
+        {
+            get
+            {
+                return DifficultiesAvail == 0 || DifficultiesAvail == 2;
+            } 
+        }
+        public bool AdvancedAvailable
+        {
+            get 
+            {
+                return DifficultiesAvail >= 1;
+            }
+        }
+        public int DifficultyEasyBorder
+        {
+            get
+            {
+                return Difficulty == 0 ? 5 : 0;
+            }
+        }
+        public int DifficultyAdvancedBorder
+        {
+            get
+            {
+                return Difficulty == 1 ? 5 : 0;
+            }
+        }
+    }
+}
--- CookEasy/ViewModels/HomePageViewModel.cs
+++ CookEasy/ViewModels/HomePageViewModel.cs
@@ -13,12 +13,33 @@ namespace CookEasy.ViewModels
         public HomePageViewModel(INavigation navigation)
         {
             GoToRecipe = new Command(OnGoToRecipe);
+<<<<<<< Updated upstream
             this.Navigation = navigation;
+=======
+            RefreshCommand = new AsyncCommand(Refresh);
+            LoadMoreCommand = new Command(LoadMore);
+
+            RecipeRecomms = new ObservableRangeCollection<RecipeProp>();
+
+            LoadMore();
+>>>>>>> Stashed changes
         }
 
         public INavigation Navigation { get; set; }
         public ICommand GoToRecipe { get; }
 
+<<<<<<< Updated upstream
+=======
+        public AsyncCommand RefreshCommand { get; }
+        public AsyncCommand<object> SelectedCommand { get; }
+
+        public Command LoadMoreCommand { get; }
+        public Command ClearCommand { get; }
+
+        public string[] cardColors = new string[9] {
+            "#faced0", "#c74a6d", "#88697c", "#6da6bf", "#cee0f4", "#99c0c3", "#8cb173", "#cfce94", "#ffde99" };
+
+>>>>>>> Stashed changes
         string recipeText = "Cake";
         public string RecipeText
         {
@@ -41,7 +62,24 @@ namespace CookEasy.ViewModels
 
         public async void OnRefreshingPage()
         {
+<<<<<<< Updated upstream
             // refresh page content, call this method from code behind
+=======
+            if (RecipeRecomms.Count >= 8)
+                return;
+
+            var image = "TestRecipeImage.jpeg";
+            RecipeRecomms.Add(new RecipeProp { Name = "Sip of Sunshine", Image = image, Difficulty = 1, DifficultiesAvail = 2, Likes = 129, CardColor = RandomColorPicker() });
+            RecipeRecomms.Add(new RecipeProp { Name = "Potent Potable", Image = image, Difficulty = 0, DifficultiesAvail = 0, Likes = 82, CardColor = RandomColorPicker() });
+            RecipeRecomms.Add(new RecipeProp { Name = "Potent Potable", Image = image, Difficulty = 0, DifficultiesAvail = 2, Likes = 14, CardColor = RandomColorPicker() });
+            RecipeRecomms.Add(new RecipeProp { Name = "Kenya Kiambu Handege", Image = image, Difficulty = 1, DifficultiesAvail = 1, CardColor = RandomColorPicker() });
+        }
+
+        string RandomColorPicker()
+        {
+            Random random = new Random();
+            return cardColors[random.Next(0, cardColors.Length)];
+>>>>>>> Stashed changes
         }
     }
 }
